4a6b961081479b38f019133919b4c4096f1d4efc
Your Name <mexx-363@yandex.ru>
2021-11-16 22:18:31 +0300

    5 day

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -2,6 +2,12 @@ package day5;
 
 public class Task1 {
     public static void main(String[] args) {
-
+        Car car = new Car();
+        car.setModel("Ford");
+        car.setColor("Black");
+        car.setYear(2010);
+        System.out.println(car.getModel());
+        System.out.println(car.getColor());
+        System.out.println(car.getYear());
     }
 }
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -3,6 +3,8 @@ package day5;
 
 public class Task2 {
     public static void main(String[] args) {
+        Bicycle bicycle=new Bicycle("Honda","Yellow",2015);
+        System.out.println(bicycle.getModel()+","+bicycle.getColor()+","+bicycle.getYear());
 
     }
 }
